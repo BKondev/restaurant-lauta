0b5318fee334c6ff9e06b93564c5c9a83d4ab53d
Your Name <kondevblagoslav@gmail.com>
2026-03-02 15:22:13 +0200

    Configure LAUTA restaurant

---
+++ public/restaurant-config.js
@@ -0,0 +1,15 @@
+﻿// ============================================
+// RESTAURANT CONFIGURATION - LAUTA
+// This file configures which restaurant this web copy belongs to
+// ============================================
+
+const RESTAURANT_CONFIG = {
+    id: 'rest_lauta_002',
+    name: 'LAUTA',
+    apiKey: 'beb92b8c839069ac446e0b46fa9cae43412e508cf7808cd4425f183c8b341bd5'
+};
+
+// Export for use in other scripts
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = RESTAURANT_CONFIG;
+}
